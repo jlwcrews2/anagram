9caf9ea3e61dbe986a684e82eebc13e6df611316
Jason Williams <jlwcrews@gmail.com>
2023-09-07 23:31:33 +0200

    initial commit

---
+++ src/main/java/Anagram.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Anagram {
+
+    Map<String, ArrayList<String>> findAnagrams(List<String> wordList) {
+        Map<String, ArrayList<String>> result = new HashMap<>();
+        List<AlphabetizedWord> alphabetizedWordList = createAlphabetizedWordList(wordList);
+
+        alphabetizedWordList.forEach(word -> result.put(word.originalWord(), new ArrayList<>()));
+
+        for (AlphabetizedWord word : alphabetizedWordList) {
+            alphabetizedWordList.stream()
+                    .filter(w -> w.alphabetizedWord().equals(word.alphabetizedWord()))
+                    .filter(w -> !w.originalWord().equals(word.originalWord()))
+                    .forEach(w -> {
+                        ArrayList<String> anagramList = result.get(word.originalWord());
+                        anagramList.add(w.originalWord());
+                        result.put(word.originalWord(), anagramList);
+                    });
+        }
+
+        result.forEach((key, value) -> System.out.printf("key: %s, value: %s%n", key, value));
+
+        return result;
+    }
+
+    List<AlphabetizedWord> createAlphabetizedWordList(List<String> wordList) {
+        return wordList.stream().map(word -> {
+            char[] alphaWord = word.toCharArray();
+            Arrays.sort(alphaWord);
+            String sortedWord = new String(alphaWord);
+            return new AlphabetizedWord(sortedWord, word);
+        }).sorted().toList();
+    }
+}
+
+record AlphabetizedWord(String alphabetizedWord, String originalWord) implements Comparable<AlphabetizedWord> {
+
+    @Override
+    public int compareTo(AlphabetizedWord w) {
+        return this.alphabetizedWord.compareTo(w.alphabetizedWord);
+    }
+}
---
+++ src/test/java/AnagramTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
+public class AnagramTest {
+
+    @Test
+    void ShouldReturnNoMatches() {
+        Anagram anagram = new Anagram();
+
+        Map<String, ArrayList<String>> result = anagram.findAnagrams(Arrays.asList("word", "letter", "biscuit"));
+        assert (result.get("word").isEmpty());
+    }
+
+    @Test
+    void shouldReturnPositiveResults() {
+        Anagram anagram = new Anagram();
+        Map<String, ArrayList<String>> result = anagram.findAnagrams(Arrays.asList("least", "slate", "steal", "tales", "team", "meat", "biscuit"));
+        assert (result.get("steal").size() == 3);
+    }
+}
